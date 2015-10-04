c7b3ea90ed60da784ce233f31c60e824daaca8b2
helenetangen <helene.tangen@gmail.com>
2015-10-04 12:24:56 +0200

    Made the Node class

--- Linked-List/src/linkedlist/LinkedList.java
+++ Linked-List/src/linkedlist/LinkedList.java
@@ -1,5 +1,46 @@
 package linkedlist;
 
+
 public class LinkedList {
 
+	
+	public class Node {
+		private int id;
+		private Node next;
+		private Node previous;
+		
+		public Node(int id){
+			this.id = id;
+			this.next = null;
+			this.previous = null;
+		}
+		
+		public Node(int id, Node next, Node prevous){
+			this.id = id;
+			this.next = next;
+			this.previous = prevous;
+		}
+		
+		public int getId(){
+			return this.id;
+		}
+		
+		public Node getNext(){
+			return this.next;
+		}
+		
+		public void setNext(Node newNext){
+			this.next = newNext;
+		}
+		
+		public Node getPrevious(){
+			return this.previous;
+		}
+		
+		public void setPrevious(Node newPrevious){
+			this.previous = newPrevious;
+		}
+	}
+	
+	
 }
